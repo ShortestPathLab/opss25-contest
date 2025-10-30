49cc47bab636d222363412cec7e06fe5f21375ac
Kevin <kzhe0012@student.monash.edu>
2025-10-30 19:16:13 +1100

    Add warning for not being in the right conda environment

--- scripts/opss25-planviz.cmd
+++ scripts/opss25-planviz.cmd
@@ -8,5 +8,10 @@ REM Configuration
 REM ===============================
 SET "DIR=%USERPROFILE%\opss25\packages\PlanViz\external\PlanViz"
 
+IF NOT "%CONDA_DEFAULT_ENV%" == "%ENV_NAME%" (
+        ECHO You must be in the %ENV_NAME% conda environment. Please activate it first by running 'conda deactivate %ENV_NAME%'. Aborting.
+        EXIT /B 1
+)
+
 REM Run Python script with all arguments passed to this CMD script
 python "%DIR%\script\run.py" %*
